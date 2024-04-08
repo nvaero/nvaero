17b8834f1419f9da37953873cb615bd8afdfa0a6
VIORICA VERGEADE <vioricavergeade@macbook-pro-de-viorica.home>
2024-04-08 21:34:44 +0200

    quatrieme commit

--- script.js
+++ script.js
@@ -47,6 +47,81 @@ closeButton.addEventListener('click', () => {
 
 
 
+function calculate2() {
+    var consommation = parseFloat(document.getElementById('consommation').value);
+    var tempsRoulage = parseFloat(document.getElementById('tempsRoulage').value);
+    var tempsDelestage = parseFloat(document.getElementById('tempsDelestage').value);
+    var tempsIntegration = parseFloat(document.getElementById('tempsIntegration').value);
+    var tempsDegagement = parseFloat(document.getElementById('tempsDegagement').value);
+    var tempsMarge = parseFloat(document.getElementById('tempsMarge').value);
+    var tempsCdb = parseFloat(document.getElementById('tempsCdb').value);
+    
+
+    // Ajoutez ici la logique pour ajouter les minutes en fonction du type de vol sélectionné
+    var minutesSupplementaires = 0;
+    var boutonVolLocal = document.querySelector('input[name="typeVol"][value="local"]');
+    var boutonNavigationJour = document.querySelector('input[name="typeVol"][value="jour"]');
+    var boutonNavigationNuit = document.querySelector('input[name="typeVol"][value="nuit"]');
+
+    if (boutonVolLocal.checked) {
+        minutesSupplementaires = 10;
+    } else if (boutonNavigationJour.checked) {
+        minutesSupplementaires = 30;
+    } else if (boutonNavigationNuit.checked) {
+        minutesSupplementaires = 45;
+    }
+    // Calcul du temps total en minutes
+    var tempsTotal = tempsRoulage+ tempsDelestage + tempsIntegration + tempsDegagement + tempsMarge + tempsCdb;
+    tempsTotal += minutesSupplementaires;
+    // Ajoutez ici les autres temps
+
+    // Conversion du temps total en heures
+    var tempsTotalHeures = tempsTotal / 60;
+
+    // Calcul du carburant nécessaire
+    var carburantNecessaire = tempsTotalHeures * consommation;
+
+    // Affichage des résultats
+    document.getElementById('result').innerHTML = "Temps carburant : " + tempsTotalHeures.toFixed(2) + " heures<br>Nombre de litres minimum à emmener : " + carburantNecessaire.toFixed(2) + " litres";
+    var popup = document.getElementById('popup');
+    var resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = "Temps carburant : " + tempsTotalHeures.toFixed(2) + " heures<br>Nombre de litres minimum à emmener : " + carburantNecessaire.toFixed(2) + " litres";
+    openPopup();
+}
+function openPopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = "block";
+}
+
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = "none";
+}
+
+function resetForm() {
+    // Réinitialisation des champs de saisie
+    document.getElementById('consommation').value = "";
+    document.getElementById('tempsRoulage').value = "";
+    document.getElementById('tempsDelestage').value = "";
+    document.getElementById('tempsIntegration').value = "";
+    document.getElementById('tempsDegagement').value = "";
+    document.getElementById('tempsMarge').value = "";
+    document.getElementById('tempsCdb').value = "";
+    
+    // Réinitialisation des boutons radio
+    var boutons = document.querySelectorAll('input[name="typeVol"]');
+    boutons.forEach(function(bouton) {
+        bouton.checked = false;
+    });
+    // Fermeture de la fenêtre pop-up
+    closePopup();
+}
+
+
+
+
+
+
 
 
 
